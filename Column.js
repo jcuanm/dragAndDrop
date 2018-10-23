b8924d9f623aa19011919c1558a811cf30e349f8
Javier Cuan-Martinez <javier.cuanmartinez@Javiers-MacBook-Pro.local>
2018-10-23 11:58:18 -0400

    Can retrieve the xy coordinates of a Row component

--- Column.js
+++ Column.js
@@ -11,120 +11,50 @@ import Row from "./Row";
 class Column extends Component{
     constructor(props){
         super(props);
-        
         this.state = {
             rows: []
         }
+        this.updateRows = this.updateRows;
     }
 
+    /* Add a new row to the column */
     addRow(){
-        //let {numRows, rows} = this.props.rows;
         const layout = new Animated.ValueXY();
-        //const newRow = new Row(numRows, layout);
-        //newRow.setRenderStatus(true);
-        const panResponder = PanResponder.create({
-
-            /* Indicate to OS that we want movement for this panresponder */
-            onMoveShouldSetResponderCapture: () => true,
-
-            /* Indicate to the OS that we want dragging movement */
-            onMoveShouldSetPanResponderCapture : () => true,
-
-            /* Set initial position */
-            onPanResponderGrant: () => {
-                //this.currActiveRow = newRow;
-                layout.setValue({x: 0, y: 0});
-                //console.log("Granted", this.currActiveRow.index);
-            },
-
-            /* On movement logic */
-            onPanResponderMove : (e, gestureState) => {
-                // const lowerNeighbor = this.state.data[0].rows[newRow.index - 1];
-                // const higherNeighbor = this.state.data[0].rows[newRow.index + 1];
-                // let newYCoord = newRow.coords.y + gestureState.dy;
-                // let newXCoord = newRow.coords.x + gestureState.dx;
-
-                // if (newYCoord <= lowerNeighbor.coords.y && newXCoord == lowerNeighbor.coords.x){
-                //     newRow.setRenderStatus()
-                //     let newRowsList = this.swap(this.state.data[0].rows, newRow.index, newRow.index - 1);
-                    
-                //     let updatedData = {
-                //     id: columnId,
-                //     name: columnName,
-                //     numRows: numRows,
-                //     rows: newRowsList
-                //     }
-
-                //     this.setState({
-                //     data : [updatedData]
-                //     });
-                // }
-
-                Animated.event([null,{
-                        dx : layout.x,
-                        dy : layout.y
-                }])(e, gestureState);
-            },
-
-            /* On release logic */
-            onPanResponderRelease : (e, gesture) => {
-                this.currActiveRow = null;
-                        Animated.spring(
-                        layout,
-                        {toValue:{x:0, y:0}}
-                        ).start();
-                layout.flattenOffset();
-                //console.log("Released", this.currActiveRow);
-                return true; 
-            }
-        });
-
-        //newRow.setPanResponder(panResponder);
-
-        // let updatedData = {
-        //     id: columnId,
-        //     name: columnName,
-        //     numRows: ++numRows,
-        //     rows: [...rows, newRow]
-        // }
         const newRow = {
             index: this.state.rows.length,
             layout: layout,
-            panResponder: panResponder
         }
-
-        /* Update the state variable */
         this.setState({
             rows : [...this.state.rows, newRow]
         });
     }
 
+    /* Create a callback function to allow the child components (Rows) to update the state */
+    updateRows(rows){
+        this.setState({rows : rows});
+    }
+
     render(){
+        //console.log(this.ref1);
         return (
             <View>
-                <View
-                    style={styles.dropZone}
-                >
-                    <Text 
-                        style={styles.text}
-                    >
+                <View style={styles.dropZone}>
+                    <Text style={styles.text}>
                         TODO List
                     </Text>
                      
                     {this.state.rows.map( row => (
-                        //this.renderDraggable(row)
                         <Row
                             key={row.index}
                             index={row.index}
                             layout={row.layout}
-                            panResponder={row.panResponder}
+                            rows={this.state.rows}
+                            updateRows={this.updateRows}
                         />
                     ))}
 
                     <Button
-                        onPress={() => {
-                        this.addRow()
-                        }}
+                        onPress={() => { this.addRow() }}
                         title="Add item"
                         style={styles.addBtn}
                     />
--- Row.js
+++ Row.js
@@ -7,28 +7,104 @@ import {
 } from 'react-native';
 
 class Row extends Component{
+    constructor(props){
+        super(props);
+        this.panResponder = this.setPanResponder(this.props.layout);
+        this.coords = {x: null, y: null};
+    }
 
     shouldComponentUpdate(nextProps){
         return this.props.index !== nextProps.index;
     }
     
-    // setCoords(xCoord,yCoord){
-    //     this.coords = {x : xCoord, y : yCoord};
-    // }
+    setPanResponder(layout){
+        const panResponder = PanResponder.create({
 
-    // setRenderStatus(bool){
-    //     this.shouldRender = bool;
-    // }
+            /* Indicate to OS that we want movement for this panresponder */
+            onMoveShouldSetResponderCapture: () => true,
+
+            /* Indicate to the OS that we want dragging movement */
+            onMoveShouldSetPanResponderCapture : () => true,
+
+            /* Set initial position */
+            onPanResponderGrant: () => {
+                console.log("Granted", this.props.rows);
+                //this.currActiveRow = newRow;
+                layout.setValue({x: 0, y: 0});      
+            },
+
+            /* On movement logic */
+            onPanResponderMove : (e, gestureState) => {
+                console.log(this.coords.x + gestureState.dx, this.coords.y + gestureState.dy);
+                // const lowerNeighbor = this.state.data[0].rows[newRow.index - 1];
+                // const higherNeighbor = this.state.data[0].rows[newRow.index + 1];
+                // let newYCoord = newRow.coords.y + gestureState.dy;
+                // let newXCoord = newRow.coords.x + gestureState.dx;
+
+                // if (newYCoord <= lowerNeighbor.coords.y && newXCoord == lowerNeighbor.coords.x){
+                //     newRow.setRenderStatus()
+                //     let newRowsList = this.swap(this.state.data[0].rows, newRow.index, newRow.index - 1);
+                    
+                //     let updatedData = {
+                //     id: columnId,
+                //     name: columnName,
+                //     numRows: numRows,
+                //     rows: newRowsList
+                //     }
+
+                //     this.setState({
+                //     data : [updatedData]
+                //     });
+                // }
+
+                Animated.event([null,{
+                        dx : layout.x,
+                        dy : layout.y
+                }])(e, gestureState);
+            },
+
+            /* On release logic */
+            onPanResponderRelease : (e, gesture) => {
+                this.currActiveRow = null;
+                        Animated.spring(
+                        layout,
+                        {toValue:{x:0, y:0}}
+                        ).start();
+                layout.flattenOffset();
+                return true; 
+            }
+        });
+
+        return panResponder
+    }
+
+    setCoords(x, y) {
+        this.coords.x = x;
+        this.coords.y = y;
+    }
+
+    /* Swap two elements from the rows list */
+    swap(rowsList, index1, index2){
+        console.log("Swap");
+        let temp = rowsList[index1];
+        rowsList[index1] = rowsList[index2];
+        rowsList[index2] = temp;
+        return rowsList;
+    }
 
     render(){
         console.log("Rendering Row: ", this.props.index);
+        
         return (
             <View 
                 key={this.props.index} 
                 style={styles.draggableContainer}
+                onLayout={({ nativeEvent}) => {
+                    this.setCoords(nativeEvent.layout.x, nativeEvent.layout.y);
+                }}
             >
                 <Animated.View 
-                    {...this.props.panResponder.panHandlers}
+                    {...this.panResponder.panHandlers}
                     style={[this.props.layout.getLayout(), 
                     styles.rectangle]}
                 >
@@ -37,9 +113,7 @@ class Row extends Component{
             </View>
         );
     }
-    //onLayout={({ nativeEvent}) => {
-            //    row.setCoords(nativeEvent.layout.x, nativeEvent.layout.y);
-            //}}
+    
 }
 
 export default Row;

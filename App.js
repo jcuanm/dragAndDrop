468d63de6bd6836595eb25b065c2a349a1fe58e3
Javier Cuan-Martinez <javier.cuanmartinez@Javiers-MacBook-Pro.local>
2018-12-18 09:59:45 -0500

    Cleaned up the code and directory

--- App.js
+++ App.js
@@ -1,71 +1,10 @@
 import React, {Component} from "react";
-import {ScrollView, View, Animated} from 'react-native';
-import Column from './Column';
-import styles from './dragAndDropStyles';
-import ViewOverflow from 'react-native-view-overflow';
-const AnimatedViewOverflow = Animated.createAnimatedComponent(ViewOverflow);
+import Board from "./src/components/Board";
 
 export default class App extends Component {
-  constructor(props){
-    super(props);
-
-    this.scroll = this.scroll.bind(this);
-    this.toggleScroll = this.toggleScroll.bind(this);
-
-    /* The state's data entry will be a list of objects representing columns */
-    this.state = {
-      shouldscroll: true,
-      columns: [
-        {
-          id: 0,
-          name: "Column 0" 
-        },
-
-        {
-          id: 1,
-          name: "Column 1" 
-        },
-
-        {
-          id: 2,
-          name: "Column 2" 
-        },
-      ],
-    };
-  }
-
-  scroll(x, y){
-    this.scroller.scrollTo({x: x + 100, y: 0});
-  }
-
-  toggleScroll(isScrolling){
-    const scrollingState = isScrolling ? false : true;
-    this.setState({shouldscroll: scrollingState});
-  }
-
-  /* Render all of the components of the View */
   render() {
     console.log("Rendering Board");
 
-    return (
-      <ScrollView
-        style={{overflow:'visible'}}
-        horizontal
-        scrollEnabled={this.state.shouldscroll}
-        ref={scroller => {this.scroller = scroller}}
-      >
-        
-        {this.state.columns.map( column => ( 
-          <Column
-            key={column.id}
-            id={column.id}
-            name={column.name}
-            scroll={this.scroll}
-            toggleScroll={this.toggleScroll}
-          />  
-        ))}
-        
-      </ScrollView>
-    ); 
+    return ( <Board /> ); 
   }
 }
---
+++ ios/build/Build/Intermediates.noindex/RNViewOverflow.build/Debug-iphonesimulator/RNViewOverflow.build/Objects-normal/x86_64/RNViewOverflow.d
@@ -0,0 +1,5 @@
+dependencies: \
+  /Users/javier.cuanmartinez/Documents/360_projects/react_native_projects/appDev/dndPrototypes/dragAndDrop/node_modules/react-native-view-overflow/ios/RNViewOverflow.m \
+  /Users/javier.cuanmartinez/Documents/360_projects/react_native_projects/appDev/dndPrototypes/dragAndDrop/node_modules/react-native-view-overflow/ios/RNViewOverflow.h \
+  ../../react-native/React/Base/RCTBridgeModule.h \
+  /Users/javier.cuanmartinez/Documents/360_projects/react_native_projects/appDev/dndPrototypes/dragAndDrop/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTDefines.h
